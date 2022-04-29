5b9ebf1d760a6866a8821cb7f70607ebe1cb752e
Younes Berrada <younes.berrada@M-JCJW966J31.attlocal.net>
2022-04-29 13:33:03 -0400

    added python script-cronjob

--- cronjob.py
+++ cronjob.py
@@ -1,3 +1,15 @@
 import os
+import time 
+
+
+# cmd = 'mgeneratejs tracking.json -n 5 | mongoimport --uri mongodb+srv://mycluster-ABCDE.azure.mongodb.net/ \
+#    --username=MYUSERNAME \
+#    --password=SECRETPASSWORD --collection=mycollectionname'
 cmd = 'ls -l'
-os.system(cmd)
+
+while True: 
+    os.system(cmd)
+    print("Telemetry data sent")
+    time.sleep(1)
+
+
